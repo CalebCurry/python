e8418c26bb984d2f4eccd9764ab4a2d28a20d420
Caleb Curry <calebcurry@Calebs-MacBook-Pro.local>
2020-03-31 14:06:55 -0400

    looping yo

--- beginner_python/09-nested-loops.py
+++ beginner_python/09-nested-loops.py
@@ -1,5 +1,6 @@
 ########## NESTED FOR LOOP PT2 ###########
 
+
 #The previous section had this loop. 
 #The only thing we did was print j and a new line.
 for i in range(4):
@@ -46,3 +47,86 @@ for i in range(10):
 #etc. 
 
 
+########## NESTED WHILE ##########
+
+
+#We now have to worry bout the initialization and increment.
+
+#print 0-4, 4 times:
+i = 0
+while i < 4:
+    j = 0
+    while j < 5:
+        print(j, end=" ")
+        j += 1
+    print()
+    i += 1
+
+#not as clean...initialization and inrement has to be correct
+#pay close attention
+
+#This won't work as expected:
+
+print("Let's try this again:")
+i = 0
+j = 0
+while i < 4:
+    while j < 5:
+        print(j, end=" ")
+        j += 1
+    print()
+    i += 1
+
+print("What happened?") #j is never reset back to zero
+
+
+########## LOOP TO SUM ALL NUMBERS ##########
+
+
+#I want to calculate the sum of numbers 0-10: 10+9+8+7+6+5+4+3+2+1
+
+sum = 0
+j = 10
+while j > 0:
+    sum += j
+    j -= 1
+print(sum) #55 
+
+#But now I want to get the sum of 9-0, 8-0, 7-0, 6-0, etc...
+#Here will be the structure:
+
+i = 10
+while (i > -1): #include zero. Could also do >= 0
+    #print sum from i to zero. 
+    i -= 1
+
+
+#We just coded how to sum to zero...so copy/paste that there....
+#change to j = i (dynamic from 10 to 0)
+print("All the sums:")
+i = 10
+while (i > -1): 
+    sum = 0
+    j = i
+    while j > 0:
+        sum += j
+        j -= 1
+    print(sum) 
+    i -= 1
+
+#We can now customize the output to be more readable:
+print("All the sums with output:")
+i = 10
+print("Getting sums up to", i)
+while (i > -1): 
+    sum = 0
+    j = i
+    while j > 0:
+        if j > 1:
+            print(j, "+", end=" ")
+        else:
+            print(j, "=", end=" ")
+        sum += j
+        j -= 1
+    print(sum) 
+    i -= 1
