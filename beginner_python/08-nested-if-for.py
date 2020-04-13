0eb4e4913cc1b7dfc5481a1108e3ab641a7c4c74
Caleb Curry <calebcurry@Calebs-MacBook-Pro.local>
2020-04-13 07:42:01 -0400

    implicit bool fix

--- beginner_python/08-nested-if-for.py
+++ beginner_python/08-nested-if-for.py
@@ -24,8 +24,8 @@ logging = True
 logging_in = True
 name = "Caleb"
 
-if logging_in == True:
-    if logging == True:
+if logging_in:
+    if logging:
         print(name + " is logging in.") #log console, file, or db.
     print("Welcome, " + name)
 
