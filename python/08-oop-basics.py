bfeb5262cf1268189cf3dfeba843c3c829f9dbce
Caleb Curry <caleb@calebcurry.com>
2025-04-04 10:32:29 -0400

    improve boolean

--- python/08-oop-basics.py
+++ python/08-oop-basics.py
@@ -179,8 +179,7 @@ class Book():
         self.pages = pages
 
     def is_short(self):
-        if self.pages < 100:
-            return True
+        return self.pages < 100:
 
     #What happens when you pass object to print?
     def __str__(self):
@@ -196,20 +195,14 @@ class Book():
 
     #If should immutable, you could do something like this. 
     #This replaces __hash__ = None
-    def __hash__(self):
-        return hash(self.title) ^ hash(self.pages) #xor with hash of attributes
-        #from Mastering Object-Oriented Python
+    # def __hash__(self):
+    #     return hash(self.title) ^ hash(self.pages) #xor with hash of attributes
+    #     #from Mastering Object-Oriented Python
 
 book = Book("Are You My Mother", 72)
-print(book)
 equal_book = Book("Are You My Mother", 72)
 print("Are they considered equal?", book == equal_book) #yep
 print("Are they the same object?", book is equal_book) #nope
 book2 = Book("The Digging-est Dog", 72)
 
 print(hash(book), hash(book2))
-
-print("old hash", hash(book))
-book.title = "new"
-print("new hash", hash(book)) #BAD!!! 
-#Hashes shouldn't change
